21dcd365253c6faafc50e443095ab2ab2863b53d
Hindhj <hindhjhajji@gmail.com>
2023-04-10 01:02:43 -0700

    Library

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isalpha - Shows 1 if the input is a
+ * letter Another cases, shows 0
+ *
+ * @c: The number to be checked
+ *
+ * Return: 1 for letters. 0 for the rest.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - coputes the absolute value of a num
+ * @n: the number being checked
+ *
+ * Return: the num as absolute
+ */
+int _abs(int n)
+{
+	int num = n;
+
+	if (n < 0)
+		return (num * -1);
+	else
+		return (num);
+}
