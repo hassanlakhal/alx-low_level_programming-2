f5500e6cb3412330271c1fce4455b3812e50eb80
Hindhj <hindhjhajji@gmail.com>
2023-04-27 09:00:57 -0700

    up

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,13 @@
-		section .data
+section .data
 text: db "Hello, Holberton", 0
 
-		section .text
-		global main
-		extern printf
+section .text
+global main
+extern printf
+
 main:
 	mov rdi, text
-	xor rax, rax
-	call printf
 	xor eax, eax
+	call printf
 	mov eax, 0
 	ret
