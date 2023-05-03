b1fe9e808dfdbe7538f097d8d6f0b7ddf09b780c
Hindhj <hindhjhajji@gmail.com>
2023-05-02 19:04:51 -0700

    F

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,29 +13,28 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int md;
 	listint_t *hin;
-	listint_t *hdj;
+	listint_t *next;
 
 	if (head == NULL || *head == NULL)
 			return (-1);
 
 	if (index == 0)
 	{
-		hdj = (*head)->next;
+		next = (*head)->next;
 		free(*head)
 		*head = next;
 		return (1);
 	}
 	hin = *head;
-
 	for (md = 0; md < index - 1; md++)
 	{
 		if (hin->next == NULL)
 			return (-1);
 		hin = hin->next;
 	}
-	hdj = hin->next;
-	hin->next = hdj->next;
-	free(hdj);
+	next = hin->next;
+	hin->next = next->next;
+	free(next);
 
 	return (1);
 }
